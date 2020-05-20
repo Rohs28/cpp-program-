04ae2332aaa02129ffbc0beb47195275720d2428
Rohs28 <65682547+Rohs28@users.noreply.github.com>
2020-05-21 01:45:40 +0530

    Add files via upload
    This file will print size of different built in data types.

---
+++ size of datatype.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{int intType;float floatType;char charType; double doubleType;
+
+	;
+	cout<<"size of int = "<<sizeof(intType)<<" bytes"<<"\n"<<"size of float = "<<sizeof(floatType)<<" bytes"<<"\n"<<"size of char = "<<sizeof(charType)<<" byte"<<"\n"<<"size of double = "<<sizeof(doubleType)<<" bytes"<<"\n";
+	return 0;
+}
