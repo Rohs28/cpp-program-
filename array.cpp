00f525e6a51b68f9f81ca97b0d7577281f4d2f92
Rohs28 <65682547+Rohs28@users.noreply.github.com>
2020-05-29 20:28:36 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int arr[5];
+	int i=0;
+		cout<<"Enter elements :"<<endl;
+	
+	for(int i=0;i<5;i++)
+	{
+			cin>>arr[i];
+		}
+		for(i=0;i<5;i++)
+		{
+		
+cout<<"You entered :"<<arr[i]<<endl;
+}
+return 0;
+}
